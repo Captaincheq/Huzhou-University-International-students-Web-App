161d9ac5e5484b1d7e819fa2fed82bc5dea0b495
Captaincheq <56479455+Captaincheq@users.noreply.github.com>
2019-12-15 05:26:49 +0800

    update

---
+++ js/contact-form-script.js
@@ -0,0 +1,41 @@
+//lets see
+console.log('It works')
+$(document).ready(function () {
+    $('.submit').click(function (event) {
+        console.log('clicked button')
+
+        var name = $('.name').val()
+        var email = $('.email').val()
+        var subject = $('.subject').val()
+        var message = $('.message').val()
+        var statusElm = $('.status')
+        statusElm.empty()
+
+        if(name.length >= 2) {
+            statusElm.append('<div>name is valid</div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>name is not valid</div>') 
+        }
+        
+        if(email.length > 5 && email.includes('@') && email.includes('.')) {
+            statusElm.append('<div>Email is valid</div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Email is not valid</div>') 
+        }
+
+        if(subject.length >= 2) {
+            statusElm.append('<div>Subject is valid</div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>subject is not valid</div>') 
+        }
+        if(message.length > 10) {
+            statusElm.append('<div>Message is valid</div>')
+        } else {
+            event.preventDefault()
+            statusElm.append('<div>Message is not valid</div>')
+        }
+    })
+})
